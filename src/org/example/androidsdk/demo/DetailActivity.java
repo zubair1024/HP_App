a622a87645b62f746dfafd33fe788c52067503cc
Zubair Ahmed <zubair1024@gmail.com>
2014-06-02 02:16:42 +0400

    0.0.3

--- src/org/example/androidsdk/demo/DetailActivity.java
+++ src/org/example/androidsdk/demo/DetailActivity.java
@@ -2,6 +2,7 @@ package org.example.androidsdk.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class DetailActivity extends Activity {
 	
@@ -10,6 +11,16 @@ public class DetailActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detail);
+	
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId()== android.R.id.home) {
+			finish();
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
--- src/org/example/androidsdk/demo/MainActivity.java
+++ src/org/example/androidsdk/demo/MainActivity.java
@@ -1,8 +1,11 @@
 package org.example.androidsdk.demo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,10 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		View layout = findViewById(R.id.layout);
+		Drawable background = layout.getBackground();
+		background.setAlpha(120);
 	}
 
 	@Override
@@ -18,5 +25,11 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	public void openActivityTwo(View v)
+	{
+		Intent intent = new Intent(this, DetailActivity.class);
+		startActivity(intent);
+	}
+
 
 }
