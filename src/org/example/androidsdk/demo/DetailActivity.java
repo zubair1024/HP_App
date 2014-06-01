caca0132dea8bdd628364449707944d5be97041d
Zubair Ahmed <zubair1024@gmail.com>
2014-06-02 01:08:08 +0400

    0.0.2

---
+++ src/org/example/androidsdk/demo/DetailActivity.java
@@ -0,0 +1,15 @@
+package org.example.androidsdk.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailActivity extends Activity {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_detail);
+	}
+
+}
